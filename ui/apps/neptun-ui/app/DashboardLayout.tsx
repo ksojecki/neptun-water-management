851871932eff135bbab757f023da7d7b07d68178
Kamil Sojecki <kamil@sojecki.pl>
2025-11-16 22:53:37 +0100

    Added Modal

--- ui/apps/neptun-ui/app/DashboardLayout.tsx
+++ ui/apps/neptun-ui/app/DashboardLayout.tsx
@@ -3,9 +3,13 @@ import { Outlet} from 'react-router';
 import { CenterLayout } from '@ui/layout/centerLayout';
 import { Menu } from '@ui/menu';
 import { FaTools, FaUser } from 'react-icons/fa';
+import { Modal } from '@ui/Modal';
+import { useState } from 'react';
 
 export const DashboardLayout = () => {
   const { isLoading, user, logout } = useAuthentication();
+  const [showConfig, setShowConfig] = useState<boolean>(false);
+  const openConfig = () => setShowConfig(true);
 
   const goToGithub = () => {
     window.open('https://github.com/ksojecki/neptun-water-management', '_blank');
@@ -24,12 +28,13 @@ export const DashboardLayout = () => {
           <FaUser/>
         </Menu.Link>
         }
-        { user && <Menu.Link tooltip="Settings" onClick={console.log}>
+        { user && <Menu.Link tooltip="Settings" onClick={openConfig}>
           <FaTools />
         </Menu.Link>
         }
         <Menu.ToggleTheme tooltip={'Change theme'} />
       </Menu>
+      <Modal open={showConfig} onClosed={() => setShowConfig(false)} />
     </>
   );
 };
